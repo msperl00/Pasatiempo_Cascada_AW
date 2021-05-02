a11d4e0d08d5c34f34321d7ee090810cfdd112e4
msperl00 <msperl00@estudiantes.unileon.es>
2021-05-02 20:29:06 +0200

    Validando las palabras

--- main.js
+++ main.js
@@ -13,8 +13,58 @@ window.setInterval(function() {
 },1000);
 
 function reseteo(){
-    window.alert("Reinicioamos tiempo y pasatiempo");
-
+    window.alert("Reiniciamos tiempo y pasatiempo");
+    //Le damos la iteración al primer elemento
+    document.getElementById("1").focus();
     minutos = 0;
     segundos = 0;
 }
+
+const url = "https://ordenalfabetix.unileon.es/aw/diccionario.txt";
+let arrayData = new Array();
+var separador = "\n";
+
+// Todas las funciones con un valor async, devuelve una promesa
+async function getData(){
+
+    // Fetch devuelve un objeto promise contieniendo la respuesta en un objeto Response.
+    // Un objeto response es una respuesta de tipo HTTP, no el propio texto.
+    // Cors sirve hacer solicitudes seguras
+   fetch(url, {
+       method: 'GET',
+        mode: 'cors'
+   })
+   .then(response => response.text())
+   .then(data => {
+   arrayData =  data.split(separador);
+   });
+
+   //Salida por pantalla de la información
+   for (var i=0; i < arrayData.length; i++) {
+    console.log(arrayData[i]);
+ }
+
+}
+
+/*Añadimos un action listener para la validación de las palabras */
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("tablero").addEventListener('submit', validarPalabra); 
+  });
+
+  function validarPalabra() {
+    
+    
+
+    var elements = document.getElementsByClassName("palabra");
+  
+
+
+
+  }
+
+
+
+
+
+
+ 
